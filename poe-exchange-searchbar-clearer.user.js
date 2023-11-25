b8d1d0ce4324f650792444b625c0aabcec1030be
D4Enjoyer <146950055+D4Enjoyer@users.noreply.github.com>
2023-11-25 15:20:23 +0100

    Update poe-exchange-searchbar-clearer.user.js
    changed waitForKeyElement source
    removed jQuery

--- poe-exchange-searchbar-clearer.user.js
+++ poe-exchange-searchbar-clearer.user.js
@@ -1,25 +1,24 @@
 // ==UserScript==
 // @name         poe-exchange-searchbar-clearer
 // @namespace    https://github.com/D4Enjoyer/poe-exchange-searchbar-clearer
-// @version      1.1
+// @version      1.2
 // @description  Clears the search bar when clicking exchange items on Path of Exile Trade Exchange if it contains text
 // @author       A God Gamer with his dear friend ChatGPT
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=pathofexile.com
 // @match        https://www.pathofexile.com/trade*
 // @grant        none
-// @require      https://code.jquery.com/jquery-3.6.0.min.js
-// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @require      https://cdn.jsdelivr.net/gh/CoeJoder/waitForKeyElements.js@v1.2/waitForKeyElements.js
 // ==/UserScript==
 
 // Function to clear the search bar and trigger input event
 function clearSearchBar() {
     console.log('Clearing search bar...');
-    var searchBar = $('.search-select.form-control.text');
+    var searchBar = document.querySelector('.search-select.form-control.text');
 
-    if (searchBar.length) {
-        searchBar.val(''); // Empty the search bar
-        // Trigger input event using vanilla JavaScript dispatchEvent
-        searchBar[0].dispatchEvent(new Event('input', { bubbles: true }));
+    if (searchBar) {
+        searchBar.value = ''; // Empty the search bar
+        // Trigger input event using Event constructor
+        searchBar.dispatchEvent(new Event('input', { bubbles: true }));
         console.log('Search bar cleared.');
     } else {
         console.log('Search bar not found.');
@@ -28,15 +27,15 @@ function clearSearchBar() {
 
 // Function to check if the search bar contains text
 function shouldClearSearchBar() {
-    var searchBar = $('.search-select.form-control.text');
-    var searchBarContainsText = searchBar.length && searchBar.val().trim() !== '';
+    var searchBar = document.querySelector('.search-select.form-control.text');
+    var searchBarContainsText = searchBar && searchBar.value.trim() !== '';
     console.log('Search bar contains text:', searchBarContainsText);
     return searchBarContainsText;
 }
 
 // Event handler for clicking on exchange items
 function handleExchangeItemClick(event) {
-    if ($(event.target).closest('.exchange-filter-item').length && shouldClearSearchBar()) {
+    if (event.target.closest('.exchange-filter-item') && shouldClearSearchBar()) {
         console.log('Clicked on .exchange-filter-item');
         clearSearchBar(); // Call the function to clear the search bar if it contains text
     }
@@ -51,7 +50,7 @@ function initializeScript() {
         console.log('Search bar element found.');
 
         // Add event listener for clicks on exchange items
-        $('.search-advanced-items.exchange').on('click', handleExchangeItemClick);
+        document.querySelector('.search-advanced-items.exchange').addEventListener('click', handleExchangeItemClick);
 
         console.log('Script initialized.');
     });
